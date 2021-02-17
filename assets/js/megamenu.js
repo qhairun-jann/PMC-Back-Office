110ac14e67c282565bcc459a417a3f34eb545bcc
qhairun-jann <73168030+qhairun-jann@users.noreply.github.com>
2021-02-17 12:26:15 +0800

    update

---
+++ assets/js/megamenu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('nav .button').click(function(){
+    $('nav .button span').toggleClass("rotate");
+    });
+    $('nav ul li .first').click(function(){
+    $('nav ul li .first span').toggleClass("rotate");
+    });
+    $('nav ul li .second').click(function(){
+    $('nav ul li .second span').toggleClass("rotate");
+    });
+    
+    });
