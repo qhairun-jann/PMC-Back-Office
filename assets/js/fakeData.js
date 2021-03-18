0557a50feb85a131291316f3a3e4128a913fc06f
ramrajg1610 <79183446+ramrajg1610@users.noreply.github.com>
2021-03-18 18:10:12 +0800

    filter
    filter

--- assets/js/fakeData.js
+++ assets/js/fakeData.js
@@ -1,124 +1,194 @@
-const tourRequest = { category: "category", item: "tour request" };
-const offers = { category: "category", item: "offers" };
-const user_reg = { category: "category", item: "user registeration" };
-const requirments = { category: "category", item: "requirements" };
+// #Category
+const card_1 = {
+  id: "1",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "appointments",
+  p: "appointments",
+  filterNames: ["appointments"]
+}
 
-const bangsar = { category: "location", item: "bangsar" };
-const kl_gateway = { category: "location", item: "kl Gateway Residences" };
-const kl_eco = { category: "location", item: "kl eco city" };
-const block_a = { category: "unit", item: "block A" };
-const basement = { category: "unit", item: "basement" };
-const a_g_1 = { category: "unit", item: "a-g-1" };
+const card_2 = {
+  id: "2",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "offers",
+  p: "offers",
+  filterNames: ["offers"]
+}
 
-//date format : DD/MM/YYYY
-const activities = [
-  {
-    id: 11111,
-    date: "13/02/2021",
-    dateElement: `<small class="text-muted">13 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2>1 New Tour Request </h2>`,
-    descriptionElement: `<p>Jeremy Davidson wants to tour a unit in Paradise Residence</p>`,
-    filterKey: [tourRequest],
-    link: "#",
-  },
+const card_3 = {
+  id: "3",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "payments",
+  p: "payments",
+  filterNames: ["payments"]
+}
 
-  {
-    id: 22222,
-    date: "14/02/2021",
-    dateElement: `<small class="text-muted">14 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> An appointment for property viewing </h2>`,
-    descriptionElement: `<p>appointment Kl Eco City </p>`,
-    filterKey: [kl_eco],
-    link: "#",
-  },
+// #Building
+const card_4 = {
+  id: "4",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "kl_gateway_residenes",
+  p: "kl_gateway_residenes",
+  filterNames: ["kl_gateway_residenes"]
+}
 
-  {
-    id: 33333,
-    date: "13/02/2021",
-    dateElement: `<small class="text-muted">13 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2>1 New Offers </h2>`,
-    descriptionElement: `<p>Jeremy Davidson wants Offers</p>`,
-    filterKey: [offers],
-    link: "#",
-  },
+const card_5 = {
+  id: "5",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "kl_gateway_premium",
+  p: "kl_gateway_premium",
+  filterNames: ["kl_gateway_premium"]
+}
 
-  {
-    id: 44444,
-    date: "14/02/2021",
-    dateElement: `<small class="text-muted">14 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> User Register  </h2>`,
-    descriptionElement: `<p>user register </p>`,
-    filterKey: [user_reg],
-    link: "#",
-  },
+const card_6 = {
+  id: "6",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "novum_bangsar_south",
+  p: "novum_bangsar_south",
+  filterNames: ["novum_bangsar_south"]
+}
+// #Area
+const card_7 = {
+  id: "7",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "bangsar_south",
+  p: "bangsar_south",
+  filterNames: ["bangsar_south"]
+}
 
-  {
-    id: 55555,
-    date: "13/02/2021",
-    dateElement: `<small class="text-muted">13 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> Requirments </h2>`,
-    descriptionElement: `<p>requirments</p>`,
-    filterKey: [requirments],
-    link: "#",
-  },
+// #Block
+const card_8 = {
+  id: "8",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "block_a",
+  p: "block_a",
+  filterNames: ["block_a"]
+}
+const card_9 = {
+  id: "9",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "block_b",
+  p: "block_b",
+  filterNames: ["block_b"]
+}
+const card_10 = {
+  id: "10",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "block_c",
+  p: "block_c",
+  filterNames: ["block_c"]
+}
 
-  {
-    id: 66666,
-    date: "14/02/2021",
-    dateElement: `<small class="text-muted">14 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> View Bangsar </h2>`,
-    descriptionElement: `<p>Bangsar </p>`,
-    filterKey: [bangsar],
-    link: "#",
-  },
+// #Level
+const card_11 = {
+  id: "11",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "level_1",
+  p: "level_1",
+  filterNames: ["level_1"]
+}
+const card_12 = {
+  id: "12",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "level_2",
+  p: "level_2",
+  filterNames: ["level_2"]
+}
+const card_13 = {
+  id: "13",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "level_3",
+  p: "level_3",
+  filterNames: ["level_3"]
+}
 
-  {
-    id: 11223,
-    date: "13/02/2021",
-    dateElement: `<small class="text-muted">13 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> kl Gateway </h2>`,
-    descriptionElement: `<p> kl Gateway</p>`,
-    filterKey: [kl_gateway],
-    link: "#",
-  },
+// #Unit
+const card_14 = {
+  id: "14",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "a-g-1",
+  p: "a-g-1",
+  filterNames: ["a-g-1"]
+}
 
-  {
-    id: 22334,
-    date: "14/02/2021",
-    dateElement: `<small class="text-muted">14 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> Basement </h2>`,
-    descriptionElement: `<p> basement </p>`,
-    filterKey: [basement],
-    link: "#",
-  },
+const card_15 = {
+  id: "15",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "a-g-2",
+  p: "a-g-2",
+  filterNames: ["a-g-2"]
+}
 
-  {
-    id: 55443,
-    date: "13/02/2021",
-    dateElement: `<small class="text-muted">13 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2>1 unit block A</h2>`,
-    descriptionElement: `<p> unit bloack a</p>`,
-    filterKey: [block_a],
-    link: "#",
-  },
+const card_16 = {
+  id: "16",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "a-g-3",
+  p: "a-g-3",
+  filterNames: ["a-g-3"]
+}
 
-  {
-    id: 10101,
-    date: "14/02/2021",
-    dateElement: `<small class="text-muted">14 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> category: offers, unit  a-g-1, location: bangsar!  </h2>`,
-    descriptionElement: `<p> category: offers, unit  a-g-1, location: bangsar!  </p>`,
-    filterKey: [a_g_1,bangsar,offers],
-    link: "#",
-  },
+// #Mix
+const card_17 = {
+  id: "17",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "payments block A level_3 a-g-2",
+  p: "payments block A level_3 a-g-2",
+  filterNames: ["payments", "block A", "level_3", "a-g-2"]
+}
+const card_18 = {
+  id: "18",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "a-g-3",
+  p: "a-g-3",
+  filterNames: ["a-g-3", "level_1", "offers"]
+}
 
-  {
-    id: 12345,
-    date: "14/02/2021",
-    dateElement: `<small class="text-muted">14 Feb 2021 | 1.02 pm</small>`,
-    titleElement: `<h2> UNIT A-G-1 </h2>`,
-    descriptionElement: `<p> UNIT A-G-1 </p>`,
-    filterKey: [a_g_1],
-    link: "#",
-  },
-];
+const card_19 = {
+  id: "19",
+  link: "#",
+  rawDate: new Date(),
+  date: "13 Feb 2021 | 1.02pm",
+  h1: "kl_gateway_premium bangsar a-g-3",
+  p: "kl_gateway_premium bangsar a-g-3",
+  filterNames: ["kl_gateway_premium", "bangsar", "a-g-3"]
+}
+
+const cards = [card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9,
+  card_10, card_11, card_12, card_13, card_14, card_15, card_16, card_17, card_18, card_19]
+
