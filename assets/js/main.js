cf7179c4928f5c8cfba085d751928eda066726e2
qhairun-jann <73168030+qhairun-jann@users.noreply.github.com>
2021-04-06 09:22:57 +0800

    yupdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,114 +1,31 @@
-/* globals Chart:false, feather:false */
+/* main/globals  */
 
-(function () {
-    'use strict'
+var searchBox = document.querySelectorAll(".searchBox")
+var arraySearchBox = Array.from(searchBox)
+arraySearchBox.map((box) => {
+  console.log(box)
+  box.addEventListener("click", (event) => { event.stopPropagation() })
+
+
   
-    feather.replace()
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
-          ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false
-            }
-          }]
-        },
-        legend: {
-          display: false
-        }
-      }
-    })
-  })()
+  // THIS EVENT IS DETECTED BUT NOT USEFUL
+    // box.addEventListener("mouseout", (event) => { console.log("mouseout"); event.stopPropagation() })
+    // box.children[0].addEventListener("mouseout", (event) => { console.log("mouseout"); event.stopPropagation() })
+    // box.addEventListener("mouseup", (event) => { event.stopPropagation() })
+    // box.children[0].addEventListener("mouseup", (event) => { event.stopPropagation() })
 
-  // Filter Selection
-  // ===========================================================================
-  
-  filterSelection("all")
-  function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterDiv");
-    if (c == "all") c = "";
-    for (i = 0; i < x.length; i++) {
-      w3RemoveClass(x[i], "show");
-      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-  }
 
-  function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
-    }
-  }
+    //---------------------------------
 
-  function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-
-  var btnContainer = document.getElementById("FilterContainer");
-  var btns = btnContainer.getElementsByClassName("btnfilter");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-      var current = document.getElementsByClassName("current");
-      current[0].className = current[0].className.replace("current", "");
-      this.className += " current";
-    });
-  }
-
-  $("#filteroption").change(function () {
-    var selectItem = $(this).val();
-    filter(selectItem);
-  });
-  function filter(e) {
-    var regex = new RegExp('\\b\\w*' + e + '\\w*\\b');
-    $('.filterDiv').hide().filter(function () {
-      return regex.test($(this).data('name'))
-    }).show();
-    if ($("#vertical-timeline").children(":visible").length == 0) {
-      $(".empty").show();
-    } else {
-      $(".empty").hide();
-    }
-  }
+    
+  //this event not detectable
+  // box.addEventListener("drag", (event) => { console.log("drag"); event.stopPropagation() })
+  // box.addEventListener("dragleave", (event) => { console.log("dragleave"); event.stopPropagation() })
+  // box.addEventListener("drop", (event) => { console.log("drop"); event.stopPropagation() })
+  // box.addEventListener("dragover", (event) => { console.log("dragover"); event.stopPropagation() })
+  // box.addEventListener("ondrop", (event) => { console.log("dragover"); event.stopPropagation() })
+  // box.children[0].addEventListener("drag", (event) => { console.log("drag"); event.stopPropagation() })
+  // box.children[0].addEventListener("dragleave", (event) => { console.log("dragleave"); event.stopPropagation() })
+  // box.children[0].addEventListener("drop", (event) => { console.log("drop"); event.stopPropagation() })
+  // box.children[0].addEventListener("dragover", (event) => { console.log("dragover"); event.stopPropagation() })
+})
